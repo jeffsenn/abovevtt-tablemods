a7173d0ce46f8a7a21f8f04426efea12f4fd2224
Jeff Senn <jeffsenn@gmail.com>
2025-03-26 13:53:58 -0400

    new version of dice and scrollbars

--- table-mods.js
+++ table-mods.js
@@ -1,14 +1,18 @@
 /* URL pattern: https://www.dndbeyond.com/characters/*abovevtt=true */
 setTimeout(() => {
     const mt = Mousetrap;
+    $("#splash").remove(); //remove splash on new version
+    myHidePanel = () => setTimeout(() => $('#hide_rightpanel').click(), 20);
+    myHideSheetButton = () => setTimeout(() => $('#sheet_button').hide(), 2000);
     //cycle between modes of showing combat&log
     cyclelog = function () {
         setTimeout(() => $('#combat_button').click(), 20);
         if ($('#combat_tracker_inside').is(':visible')) {
             //make sure gamelog selected
             change_sidbar_tab($('#switch_gamelog'), true);
-            $('#hide_rightpanel').click();
+            myHidePanel();
         }
+        myHideSheetButton();
     };
     mt.bind('g', cyclelog); //for debug on non-Enter kbd
     mt.bind('/', cyclelog);
@@ -23,61 +27,19 @@ setTimeout(() => {
             }
         }
     });
-    //Use number keys to collect some dice to roll in a short time
-    collectSome = function (d, n) {
-        if (mt.collectdice) {
-            clearTimeout(mt.collectTimeout);
-            mt.collectdice[d] = (mt.collectdice[d] || 0) + n;
-        } else {
-            mt.collectdice = { [d]: n };
-        }
-        mt.collectTimeout = setTimeout(() => {
-            e = Object.keys(mt.collectdice)
-                .map((k) => '' + (mt.collectdice[k] || '2') + k + ' ')
-                .join('+');
-            delete mt.collectdice;
-            window.diceRoller.roll(new DiceRoll(e, 'Table Roll', 'roll'));
-        }, 750);
-    };
-    mt.bind('1', function () {
-        collectSome('d20kl1', 0);
-    });
-    mt.bind('2', function () {
-        collectSome('d20', 1);
-    });
-    mt.bind('3', function () {
-        collectSome('d20kh1', 0);
-    });
-    mt.bind('4', function () {
-        collectSome('d4', 1);
-    });
-    mt.bind('5', function () {
-        collectSome('d6', 1);
-    });
-    mt.bind('6', function () {
-        collectSome('d8', 1);
-    });
-    mt.bind('7', function () {
-        collectSome('d10', 1);
-    });
-    mt.bind('8', function () {
-        collectSome('d12', 1);
-    });
-    mt.bind('9', function () {
-        collectSome('d100', 1);
-    });
     // make sure controls are hidden at startup
     if (!$('#hide_interface_button').hasClass('unhidden')) {
         if ($('#hide_rightpanel').hasClass('point-right')) {
-            $('#hide_rightpanel').click();
+            myHidePanel();
         }
         if (is_characters_page()) {
             hide_player_sheet();
         }
-        $('.dice-toolbar').hide();
+        //$('.dice-toolbar').hide();
         $('#hide_interface_button').show().addClass('unhidden');
         $('.hideable').hide();
     }
+    myHideSheetButton();
     // wait long enough to make sure everything is settled
     // (15s is by experiment with medium complex situ)
 }, 15000);
